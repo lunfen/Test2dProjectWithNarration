10c6eadcd841939453178ffccb7869625248f6ef
Alex LunFen <lunfen@yandex.ru>
2017-09-06 14:33:58 +0300

    Initial Commit.

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    [SerializeField] float BalloonSpeed;
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();        
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * BalloonSpeed);
+    }
+
+    public void Collided()
+    {
+        StartCoroutine(CollisionEvent());
+    }
+
+    IEnumerator CollisionEvent()
+    {
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] GameObject BalloonPrefab;
+    [SerializeField] float BalloonSpawnTime;
+    [SerializeField] float NarrationChangeTime;
+    [SerializeField] Text NarrationText;
+    [SerializeField] string[] NarrationArray = new string[5];
+
+    float balloonSpawnDistance = 10.2f;
+    int narrationIndex = 0;
+
+	void OnTriggerEnter2D (Collider2D coll)
+    {
+        if (coll.tag == "balloon")
+            coll.GetComponent<Balloon>().Collided();
+	}
+
+    private void Start()
+    {
+        StartCoroutine(SpawnBalloon());
+        StartCoroutine(ChangeNarration());
+    }
+
+    IEnumerator SpawnBalloon()
+    {
+        yield return new WaitForSeconds(BalloonSpawnTime);
+        Instantiate(BalloonPrefab, Vector3.right * balloonSpawnDistance, Quaternion.identity);
+        yield return SpawnBalloon();
+    }
+
+    IEnumerator ChangeNarration()
+    {
+        if (narrationIndex >= NarrationArray.Length) narrationIndex = 0;
+        NarrationText.text = NarrationArray[narrationIndex];
+        yield return new WaitForSeconds(NarrationChangeTime);
+        narrationIndex++;
+        yield return ChangeNarration();
+    }
+}
